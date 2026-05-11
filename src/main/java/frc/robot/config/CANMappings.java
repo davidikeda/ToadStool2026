adb04fca90e5096a3cf51170c492cbf8af63a05c
davidikeda <david.ikeda@outlook.com>
2026-05-11 13:20:35 -0400

    Fix

--- src/main/java/frc/robot/config/CANMappings.java
+++ src/main/java/frc/robot/config/CANMappings.java
@@ -9,9 +9,4 @@ public class CANMappings {
   public static final int BACK_LEFT_TURN = 12;
   public static final int BACK_RIGHT_DRIVE = 13;
   public static final int BACK_RIGHT_TURN = 14;
-  public static final int PIGEON = 30;
-  public static final int FRONT_LEFT_ENCODER = 21;
-  public static final int FRONT_RIGHT_ENCODER = 22;
-  public static final int BACK_LEFT_ENCODER = 23;
-  public static final int BACK_RIGHT_ENCODER = 24;
 }
